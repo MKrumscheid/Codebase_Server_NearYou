9fdfce9928cb4fef3bf87c9290d1173ae9a9fe2b
MKrumscheid <136487365+MKrumscheid@users.noreply.github.com>
2024-06-26 21:00:58 +0200

    0.503

--- config/database.js
+++ config/database.js
@@ -1,16 +1,30 @@
 const { Sequelize } = require("sequelize");
 
-//setting up the connection to the database
-const sequelize = new Sequelize("postgres", "postgres", "123", {
-  //123 is not actually a secure password ;)
-  host: "localhost",
-  dialect: "postgres",
-  pool: {
-    max: 50, // Maximum number of connections in the pool
-    min: 0,
-    acquire: 30000, // Maximum time, in milliseconds, that pool will try to get connection before throwing error
-    idle: 50000, // Maximum time, in milliseconds, that a connection can be idle before being released
-  },
-});
+let sequelize;
 
-module.exports = sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize("postgres", "postgres", "123", {
+    //123 is not actually a secure password ;)
+    host: "localhost",
+    dialect: "postgres",
+    pool: {
+      max: 50, // Maximum number of connections in the pool
+      min: 0,
+      acquire: 30000, // Maximum time, in milliseconds, that pool will try to get connection before throwing error
+      idle: 50000, // Maximum time, in milliseconds, that a connection can be idle before being released
+    },
+  });
+
+  module.exports = sequelize;
+}
