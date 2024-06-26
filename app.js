0c7270a953755143a34a248f0024db6ba1b6f7f7
MKrumscheid <136487365+MKrumscheid@users.noreply.github.com>
2024-06-26 20:56:38 +0200

    0.502

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const messageRoutes = require("./api/messageRoutes");
 const { setupDatabase } = require("./models/index");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
